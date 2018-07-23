bdf05cde0b039c1844c78d26e4580faafed9ad43
yihanhu97 <35671831+yihanhu97@users.noreply.github.com>
2018-07-22 20:56:46 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import numpy as np
+import math
+from scipy.signal import get_window
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../software/models/'))
+import stft
+import utilFunctions as UF
+eps = np.finfo(float).eps
+
+(fs,x) = UF.wavread(inputFile)
+w = get_window(window, M)
+
+mX, pX = stft.stftAnal(x, w, N, H)
+y = stft.stftSynth(mX, pX, M, H)
+
